1872b7da699b18ff564ec197d63183fdfaf9454d
Christopher Vertigan <n3078812@SCM-88441.windows.tees.ac.uk>
2015-11-11 20:51:29 +0000

    Work for 11/11/2015
    Started on the tunnel scripts and added froglegs with his working
    animation

---
+++ AGD Project/Assets/Tunnel.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tunnel : MonoBehaviour {
+
+	private float tunnelTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			other.GetComponent<MeshRenderer>().enabled = false;
+			startCoroutine(TunnelTravel(GameObject other.gameObject));
+		}
+	}
+
+	IEnumerator TunnelTravel(GameObject traveller)
+	{
+		tunnelTime += 1.0f * Time.deltaTime;
+
+		if (tunnelTime > 3) 
+		{
+			string tunnelName = GameObject.Find("Tunnel Entrance" + Random.range(1, 5).ToString());
+			if (tunnelName == this.gameObject.name)
+			{
+
+			}
+			traveller.transform = 
+			traveller.GetComponent<MeshRenderer>().enabled = true;
+		}
+	}
+}
